bfe97ced2e03e72fd02ed68607aa5d3e08cdea47
vdmkrchkn <vdmkrchkn@gmail.com>
2018-06-16 19:20:40 +0300

    поправил скругление углов полей ввода при наборе текста

--- app/wwwroot/app-client/src/app/services/message.service.ts
+++ app/wwwroot/app-client/src/app/services/message.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -15,7 +15,10 @@ export class MessageService {
 		return this.http.post<boolean>('api/add', msg);
 	}
 
-	getMessages(): Observable<Message[]> {
-		return this.http.get<Message[]>(this.url);
+	getMessages(from?: Date, to?: Date): Observable<Message[]> {
+		// URL encoded safe search parameter if there is a search periods of dates
+		const options = from && to ?
+			{ params: new HttpParams().set('from', from.toDateString()).set('to', to.toDateString()) } : {};
+		return this.http.get<Message[]>(this.url, options);
 	}
 }
