aea2820e0847cb0b0421857247492d641c3437eb
vdmkrchkn <vdmkrchkn@gmail.com>
2018-06-25 16:50:46 +0300

    добавил mocks in GetMessages

--- app.Tests/MessagesControllerTests.cs
+++ app.Tests/MessagesControllerTests.cs
@@ -2,6 +2,7 @@ using app.Controllers;
 using app.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 
 namespace app.Tests
 {
@@ -12,7 +13,10 @@ namespace app.Tests
         public void GetMessages_NoHours_ResponseIsNull()
         {
             // Arrange
-            MessagesController messagesController = new MessagesController(null, null);
+            var mock = new Mock<IMessageService>();
+            mock.Setup(ms => ms.GetMessages(null, null))
+                .Returns< IEnumerable<Models.ViewModels.MessageView>>(null);
+            var messagesController = new MessagesController(mock.Object);
 
             // Act
             var res = messagesController.Get(null);
@@ -25,8 +29,11 @@ namespace app.Tests
         public void GetMessages_SomeHours_ResponseIsNull()
         {
             // Arrange
-            MessagesController messagesController = new MessagesController(null);
             byte hours = 2;
+            var mock = new Mock<IMessageService>();
+            mock.Setup(ms => ms.GetMessages(hours))
+                .Returns<IEnumerable<Models.ViewModels.MessageView>>(null);
+            MessagesController messagesController = new MessagesController(mock.Object);            
 
             // Act
             var res = messagesController.Get(hours);
